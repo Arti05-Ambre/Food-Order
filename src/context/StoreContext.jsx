63057ae5aa95dd48b92d70e2729abd9c4ea15e25
Arti05-Ambre <ambrearti62@gmail.com>
2026-08-22 15:25:21 +0530

    initial comment

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,23 +1,46 @@
-import { createContext } from "react";
-import{food_list}from "../assets/assets.js"
+import { createContext, useState } from 'react'
+import { food_list } from '../assets/assets.js'
+
 export const StoreContext = createContext(null)
+
 const StoreProvider = (props) => {
 
+  const [cartItems, setCartItems] = useState({})
 
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({
+        ...prev,
+        [itemId]: 1
+      }))
+    } else {
+      setCartItems((prev) => ({
+        ...prev,
+        [itemId]: prev[itemId] + 1
+      }))
+    }
+  }
 
-    const contextValue = {
-        food_list
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({
+      ...prev,
+      [itemId]: prev[itemId] - 1
+    }))
+  }
 
+  const contextValue = {
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart
+  }
 
-
-
-}
-
-return(
+  return (
     <StoreContext.Provider value={contextValue}>
-        {props.children
-        }
+      {props.children}
     </StoreContext.Provider>
-)
+  )
 }
-export default StoreProvider;
+
+export default StoreProvider
