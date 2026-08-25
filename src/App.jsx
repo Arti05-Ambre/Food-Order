1307d2348f52214eece460d27c279a3fe21dc6bc
Arti05-Ambre <ambrearti62@gmail.com>
2026-08-25 14:06:57 +0530

    initial comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
-import {Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
 const App = () => {
   return (
+    <>
     <div className='app'>
       <Navbar/>
       <Routes>
@@ -17,6 +18,8 @@ const App = () => {
       </Routes>
       
     </div>
+    <Footer  />
+    </>
   )
 }
 
