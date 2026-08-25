5f4cfb4d4f964b3a070f2709fcfdc8cff6647278
Arti05-Ambre <ambrearti62@gmail.com>
2026-08-25 15:05:22 +0530

    initial comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
@@ -8,17 +9,17 @@ import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 const App = () => {
   return (
     <>
-    <div className='app'>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/order' element={<PlaceOrder />}/>
+      <div className='app'>
+        <Navbar />
 
-      </Routes>
-      
-    </div>
-    <Footer  />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/order' element={<PlaceOrder />} />
+        </Routes>
+      </div>
+
+      <Footer />
     </>
   )
 }
