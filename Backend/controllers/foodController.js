37b841151f93e69d4fe52f02458e10d95acdb5ae
Arti05-Ambre <ambrearti62@gmail.com>
2026-09-08 15:05:22 +0530

    initial commit

--- Backend/controllers/foodController.js
+++ Backend/controllers/foodController.js
@@ -37,4 +37,23 @@ const listFood = async (req, res) => {
         res.json({success: false, message: "Error"})
     }
 }
-export { addFood, listFood }
+
+
+//remove food item
+const removeFood = async (req, res) => {
+    try {
+        const food = await foodModel.findById(req.body.id);
+        if (food) {
+            fs.unlink(`uploads/${food.image}`, () => {});
+            await foodModel.findByIdAndDelete(req.body.id);
+            res.json({success: true, message: "Food Removed"});
+        } else {
+            res.json({success: false, message: "Food not found"});
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: "Error"});
+    }
+};
+
+export { addFood, listFood, removeFood };
