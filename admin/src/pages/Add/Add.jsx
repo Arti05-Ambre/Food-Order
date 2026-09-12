09e574a6ee3e3ad860ec734ff33304fad7c2227d
Arti05-Ambre <ambrearti62@gmail.com>
2026-09-12 15:53:43 +0530

    initial commit

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -2,8 +2,12 @@
 import React, { useState } from 'react'
 import './Add.css'
 import { assets } from '../../assets/assets'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const Add = () => {
+
+  const url = "http://localhost:4000"
 
   const [image, setImage] = useState(false)
 
@@ -25,27 +29,63 @@ const Add = () => {
   }
 
   const onSubmitHandler = async (event) => {
-    event.preventDefault()
-    const fromData = new FormData();
-    formData.append("name",data.name)
-    formData.append("description",data.description)
-    formData.append("price",Number(data.price))
-    formData.append("cate",data.name)
 
+    event.preventDefault()
+
+    const formData = new FormData()
+
+    formData.append("name", data.name)
+    formData.append("description", data.description)
+    formData.append("price", Number(data.price))
+    formData.append("category", data.category)
+    formData.append("image", image)
+
+    const response = await axios.post(
+      `${url}/api/food/add`,
+      formData
+    )
+
+    if (response.data.success) {
+
+      toast.success(response.data.message)
+
+      setData({
+        name: "",
+        description: "",
+        price: "",
+        category: "Salad"
+      })
+
+      setImage(false)
+
+    } else {
+
+      toast.error(response.data.message)
+
+    }
   }
 
   return (
     <div className='add'>
-      <form className='flex-col' onSubmit={onSubmitHandler}>
+
+      <form
+        className='flex-col'
+        onSubmit={onSubmitHandler}
+      >
 
         <div className="add-img-upload flex-col">
+
           <p>Upload Image</p>
 
           <label htmlFor="image">
+
             <img
-              src={image ? URL.createObjectURL(image) : assets.upload_area}
+              src={
+                image
+                  ? URL.createObjectURL(image): assets.upload_area}
               alt=""
             />
+
           </label>
 
           <input
@@ -55,9 +95,11 @@ const Add = () => {
             hidden
             required
           />
+
         </div>
 
         <div className="add-product-name flex-col">
+
           <p>Product name</p>
 
           <input
@@ -68,9 +110,11 @@ const Add = () => {
             placeholder="Type here"
             required
           />
+
         </div>
 
         <div className="add-product-description flex-col">
+
           <p>Product description</p>
 
           <textarea
@@ -81,11 +125,13 @@ const Add = () => {
             placeholder="Write content here"
             required
           ></textarea>
+
         </div>
 
         <div className="add-category-price">
 
           <div className="add-category flex-col">
+
             <p>Product category</p>
 
             <select
@@ -93,6 +139,7 @@ const Add = () => {
               value={data.category}
               name="category"
             >
+
               <option value="Salad">Salad</option>
               <option value="Rolls">Rolls</option>
               <option value="Deserts">Deserts</option>
@@ -100,10 +147,13 @@ const Add = () => {
               <option value="Pure Veg">Pure Veg</option>
               <option value="Pasta">Pasta</option>
               <option value="Noodles">Noodles</option>
+
             </select>
+
           </div>
 
           <div className="add-price flex-col">
+
             <p>Product price</p>
 
             <input
@@ -114,15 +164,20 @@ const Add = () => {
               placeholder="$20"
               required
             />
+
           </div>
 
         </div>
 
-        <button type="submit" className="add-btn">
+        <button
+          type="submit"
+          className="add-btn"
+        >
           ADD
         </button>
 
       </form>
+
     </div>
   )
 }
--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import './List.css'
+import axios from "axios"
 const List = () => {
+
+  const url ="http://localhost:4000s"
+  const [list,setList]=useState([]);
+  const fetchlist = async ()=>{
+    const response = await axios.get()
+
+  }
   return (
     <div>
       
