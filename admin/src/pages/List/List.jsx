408f4cc02c85fc07ba112de0b8b4c339e7ac3061
Arti05-Ambre <ambrearti62@gmail.com>
2026-09-17 14:49:23 +0530

    initial commit

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,66 +1,104 @@
 
-import React, { useEffect, useState } from 'react'
-import './List.css'
-import axios from "axios"
-import { toast } from "react-toastify"
+import React, { useEffect, useState } from "react";
+import "./List.css";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-const List = () => {
-
-  const url = "http://localhost:4000"
-
+const List = (url) => {
+  
   const [list, setList] = useState([]);
 
-  const fetchlist = async () => {
+  // Fetch food list
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(`${url}/api/food/list`);
 
-    const response = await axios.get(`${url}/api/food/list`)
-
-    if (response.data.success) {
-
-      setList(response.data.data);
-
-    } else {
-
-      toast.error("Error")
-
+      if (response.data.success) {
+        setList(response.data.data);
+      } else {
+        toast.error("Error fetching food list");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
     }
-  }
+  };
 
-  const removeFood = async(foodId)=>{
-    console.log(foodID);
+  // Remove food
+  const removeFood = async (foodId) => {
+    try {
+      const response = await axios.post(
+        `${url}/api/food/remove`,
+        {
+          id: foodId,
+        }
+      );
 
-  }
+      if (response.data.success) {
+        toast.success(response.data.message);
 
+        // Refresh list after deleting
+        await fetchList();
+      } else {
+        toast.error("Error removing food");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
+  // Fetch list when component loads
   useEffect(() => {
-    fetchlist()
-  }, [])
+    fetchList();
+  }, []);
 
   return (
-    <div className='list add flex-col'>
+    <div className="list add flex-col">
       <p>All Foods List</p>
-<div className="list-table">
-  <div className="list-table-format">
-    <b>Image</b>
-    <b>Name</b>
-    <b>Category</b>
-    <b>Price</b>
-    <b>Action</b>
-  </div>
-  {list.map((item,index)=>{
-    return(
-      <div key={index} className='list-table-format'>
-        <img src={`${url}/images/` + item.image}alt=""/>
-      <p>{item.name}</p>
-      <p>{item.category}</p>
-      <p>${item.price}</p>
-      <p onClick={()=>removefood(item._id)}className='Cursor '>X</p>
+
+      <div className="list-table">
+
+        {/* Table Header */}
+        <div className="list-table-format">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+
+        {/* Food List */}
+        {list.map((item, index) => {
+          return (
+            <div key={index} className="list-table-format">
+
+              <img
+                src={`${url}/images/${item.image}`}
+                alt={item.name}
+              />
+
+              <p>{item.name}</p>
+
+              <p>{item.category}</p>
+
+              <p>${item.price}</p>
+
+              <p
+                onClick={() => removeFood(item._id)}
+                className="Cursor"
+              >
+                X
+              </p>
+
+            </div>
+          );
+        })}
+
       </div>
-
-    )
-
-  })}
-</div>
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
+
