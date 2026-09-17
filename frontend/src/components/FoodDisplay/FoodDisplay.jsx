41a894cd64ef132477cf8ad63c21dc3f991a9ecc
Arti05-Ambre <ambrearti62@gmail.com>
2026-09-17 12:50:20 +0530

    initial commit

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -6,28 +6,36 @@ import FoodItem from '../FoodItem/FoodItem'
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext)
 
+  // Selected category items first
+  const sortedFoodList = [...food_list].sort((a, b) => {
+    if (category === 'All') return 0
+
+    if (a.category === category && b.category !== category) {
+      return -1
+    }
+
+    if (a.category !== category && b.category === category) {
+      return 1
+    }
+
+    return 0
+  })
+
   return (
     <div className='food-display' id='food-display'>
       <h2>Top dishes near you</h2>
 
       <div className='food-display-list'>
-        {food_list.map((item, index) => {
-          {console.log(category,item.category);}
-          if (category==="All" || category===item.category){
-            return (
-            <FoodItem
-              key={index}
-              id={item._id}
-              name={item.name}
-              description={item.description}
-              price={item.price}
-              image={item.image}
-            />)
-
-          }
-          
-          
-        })}
+        {sortedFoodList.map((item, index) => (
+          <FoodItem
+            key={index}
+            id={item._id}
+            name={item.name}
+            description={item.description}
+            price={item.price}
+            image={item.image}
+          />
+        ))}
       </div>
     </div>
   )
