15ab1d3cefc7d4f599f617bc165f096d06bcc61e
Arti05-Ambre <ambrearti62@gmail.com>
2026-09-21 11:27:03 +0530

    initial commit

--- Backend/controllers/cartController.js
+++ Backend/controllers/cartController.js
@@ -3,6 +3,25 @@ import UserModel from "../models/UserModel.js"
 
 //add items to user cart
 const addToCart = async(req,res)=>{
+    try {
+        let userData=await UserModel.findOne({_id:req.body.userId});
+        let cartData = await userData.cartData;
+        if(!cartData[req.body.itemId])
+        {
+            cartData[req.body.itemId] = 1;
+        }
+        else{
+            cartData[req.body.itemId] += 1;
+        }
+
+        await UserModel.findByIdAndUpdate(req.body.userId,{cartData});
+        res.json({success:true,message:"Added To Cart"});
+    } catch (error) {
+console.log(error);
+res.json({success:false,message:"Error"})
+
+        
+    }
 
 
 
