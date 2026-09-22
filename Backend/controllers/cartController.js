995595e5d5f70d158f2a6a64e47a7f4c45f74d54
Arti05-Ambre <ambrearti62@gmail.com>
2026-09-22 15:01:11 +0530

    initial commit

--- Backend/controllers/cartController.js
+++ Backend/controllers/cartController.js
@@ -1,38 +1,81 @@
-import UserModel from "../models/UserModel.js"
+import UserModel from "../models/UserModel.js";
 
-
-//add items to user cart
-const addToCart = async(req,res)=>{
+// add items to user cart
+const addToCart = async (req, res) => {
     try {
-        let userData=await UserModel.findOne({_id:req.body.userId});
-        let cartData = await userData.cartData;
-        if(!cartData[req.body.itemId])
-        {
+        let userData = await UserModel.findById(req.body.userId);
+        let cartData = userData.cartData;
+
+        if (!cartData[req.body.itemId]) {
             cartData[req.body.itemId] = 1;
-        }
-        else{
+        } else {
             cartData[req.body.itemId] += 1;
         }
 
-        await UserModel.findByIdAndUpdate(req.body.userId,{cartData});
-        res.json({success:true,message:"Added To Cart"});
+        await UserModel.findByIdAndUpdate(req.body.userId, { cartData });
+
+        res.json({
+            success: true,
+            message: "Added To Cart"
+        });
+
     } catch (error) {
-console.log(error);
-res.json({success:false,message:"Error"})
-
-        
+        console.log(error);
+        res.json({
+            success: false,
+            message: "Error"
+        });
     }
+};
 
 
+// remove items from user cart
+const removeFromCart = async (req, res) => {
+    try {
+        let userData = await UserModel.findById(req.body.userId);
+        let cartData = userData.cartData;
 
-}
+        if (cartData[req.body.itemId]) {
+            cartData[req.body.itemId] -= 1;
+        }
 
-//remove items from usercart
-const removeFromCart = async(req,res)=>{
+        await UserModel.findByIdAndUpdate(req.body.userId, { cartData });
 
-}
+        res.json({
+            success: true,
+            message: "Removed From Cart"
+        });
+
+    } catch (error) {
+        console.log(error);
+
+        res.json({
+            success: false,
+            message: "Error"
+        });
+    }
+};
+
+
 // fetch user cart data
-const getCart = async(req,res)=>{
+const getCart = async (req, res) => {
+    try {
+        let userData = await UserModel.findById(req.body.userId);
+        let cartData = userData.cartData;
 
-}
-export {addToCart,removeFromCart,getCart}
+        res.json({
+            success: true,
+            cartData
+        });
+
+    } catch (error) {
+        console.log(error);
+
+        res.json({
+            success: false,
+            message: "Error"
+        });
+    }
+};
+
+export { addToCart, removeFromCart, getCart };
--- Backend/models/UserModel.js
+++ Backend/models/UserModel.js
@@ -1,11 +1,31 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name:{type:String,required:true},
-    email:{type:String,required:true,unique:true},
-    password:{type:String,required:true},
-    cartDate:{type:Object,default:{}}
-},{minimize:false})
+    name: {
+        type: String,
+        required: true
+    },
 
-const UserModel = mongoose.models.user || mongoose.model("user",userSchema);
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    cartData: {
+        type: Object,
+        default: {}
+    }
+
+}, { minimize: false });
+
+const UserModel =
+    mongoose.models.user ||
+    mongoose.model("user", userSchema);
+
 export default UserModel;
