b1afcb76b71f8df749a5d8b38d7475feba763ff9
Arti05-Ambre <ambrearti62@gmail.com>
2026-09-24 21:59:13 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
 
-  const url = "http://localhost:4000";
+  const url = "https://food-order-backend-4e0u.onrender.com";
 
   return (
     <div>
