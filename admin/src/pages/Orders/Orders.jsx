7bb5c66a87c16233f49655190649236e664a5666
Arti05-Ambre <ambrearti62@gmail.com>
2026-09-24 14:24:49 +0530

    initial commit

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react'
 import './Orders.css'
 import {useState}from 'react'
 import {toast} from "react-toastify"
-import { useEffect } from 'react'
 import axios from 'axios'
+import { assets } from '../../assets/assets'
 const Orders = ({url}) => {
 
 const [orders,setOrders] = useState([]);
@@ -27,8 +27,44 @@ const fetchAllOrders = async ()=>{
     },[])
 
   return (
-    <div>
-      
+    <div className='order add'>
+
+      <h3>Order Page</h3>
+      <div className='order-list'>
+        {orders.map((order,index)=>(
+          <div key={index} className='order-item'>
+            <img src={assets.parcel_icon} alt=""  />
+            <div>
+              <p className='order-item-food'>
+                {order.items.map((item,index)=>{
+                  if(index===order.items.length-1){
+                    return item.name + " x " +item.quantity
+
+                  }
+                  else{
+                    return item.name+ " x "+item.quantity+", "
+                  }
+
+                })}
+              </p>
+              <p className='order-item-name'>{order.address.firstName+" "+order.address.lastName}</p>
+              <div className="order-item-address">
+                <p>{order.address.street+","}</p>
+                <p>{order.address.city+", "+order.address.state+", "+order.address.country+", "+order.address.zipcode }</p>
+              </div>
+              <p className='order-item-phone'>{order.address.phone}</p>
+            </div>
+            <p>Items:{order.items.length}</p>
+            <p>${order.amount}</p>
+
+            <select>
+              <option value="Food Processing">Food Processing</option>
+              <option value="Out For Delivery">Out For Delivery</option>
+              <option value="Deliverd">Deliverd</option>
+            </select>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
