3d22c9475e1452d76b28c459b0ff460c2a5bed9c
Arti05-Ambre <ambrearti62@gmail.com>
2026-09-24 14:57:37 +0530

    initial commit

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -20,6 +20,17 @@ const fetchAllOrders = async ()=>{
      }
 
     }
+    const statusHandler =  async (event,orderId)=>{
+      const response = await axios.post(url+"/api/order/status",{
+        orderId,
+        status:event.target.value
+      })
+      if(response.data.success){
+        await fetchAllOrders();
+      }
+      
+
+    }
 
     useEffect(()=>{
       fetchAllOrders();
@@ -57,7 +68,7 @@ const fetchAllOrders = async ()=>{
             <p>Items:{order.items.length}</p>
             <p>${order.amount}</p>
 
-            <select>
+            <select onChange={(event)=>statusHandler(event,order._id)} value={order.status}>
               <option value="Food Processing">Food Processing</option>
               <option value="Out For Delivery">Out For Delivery</option>
               <option value="Deliverd">Deliverd</option>
