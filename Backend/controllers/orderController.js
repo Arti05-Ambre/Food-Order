4ebb33dd14f8593740ab60cad0051b36aace5f68
Arti05-Ambre <ambrearti62@gmail.com>
2026-09-24 14:58:20 +0530

    initial commit

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -87,4 +87,46 @@ else{
 }
 
 }
-export { placeOrder,verifyOrder };
+
+//user orders for frontend
+
+const userOrders = async(req,res)=>{
+    try {
+        const orders = await orderModel.find({userId:req.body.userId})
+        res.json({success:true,data:orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"error"})
+        
+    }
+
+}
+
+
+//Listing orders for admin panel
+const listOrders = async(req,res)=>{
+    try {
+        const orders = await orderModel.find({});
+        res.json({success:true,data:orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+        
+    }
+
+}
+
+//api updating order status
+const updateStatus = async (req,res)=>{
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status})
+        res.json({success:true,message:"Status Updated"})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+        
+    }
+
+}
+
+export { placeOrder,verifyOrder,userOrders,listOrders,updateStatus };
--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,31 +1,62 @@
 import jwt from "jsonwebtoken"
 
+
+
 const authMiddleware = async(req,res,next)=>{
+
+
 
 const{token}=req.headers;
 
+
+
 if (!token) {
+
+
 
 return res.json({success:false,message:"Not Authorixed Login Again"})
 
+
+
 }
+
+
 
 try{
 
+
+
  const token_decode = jwt.verify(token,process.env.JWT_SECRET);
+
+
 
 req.body.userId=token_decode.id;
 
+
+
  next();
+
+
 
 }catch(error){
 
+
+
 console.log(error);
+
+
 
 res.json({success:false,message:"Error"})
 
-}
+
 
 }
 
-export default authMiddleware;
+
+
+}
+
+
+
+export default authMiddleware; 
+
